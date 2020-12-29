54904dd8c0e9e9f8780a2629c77c01be26432777
Mostafa Moradian <mostafa@loadimpact.com>
2020-12-29 11:43:20 +0100

    [refactor] Fix dependencies
    1. Add dictor
    2. Remove djangorestframework-jwt
    3. Pin latest and upper versions

--- setup.py
+++ setup.py
@@ -60,7 +60,8 @@ setup(
 
     packages=find_packages(),
 
-    install_requires=['pysaml2>=4.8.0',
-                      'PyJWT>=2.0.1', ],
+    install_requires=['pysaml2>=6.4.1',
+                      'PyJWT>=2.0.0',
+                      'dictor>=0.1.7'],
     include_package_data=True,
 )

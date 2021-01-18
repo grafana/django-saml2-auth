8f72a5fee3c612c233e1255d292570b825716a9e
Mostafa Moradian <mostafa@loadimpact.com>
2021-01-18 12:45:12 +0100

    [test] Add tests for utils.py
    [test] Configure pytest

---
+++ django_saml2_auth/tests/settings.py
@@ -0,0 +1,68 @@
+import os
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+SECRET_KEY = 'SECRET'
+DEBUG = True
+ALLOWED_HOSTS = []
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'django_saml2_auth',
+]
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+ROOT_URLCONF = 'django_saml2_auth.urls'
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [BASE_DIR + '/templates'],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+WSGI_APPLICATION = 'django_saml2_auth.wsgi.application'
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+LANGUAGE_CODE = 'en-us'
+TIME_ZONE = 'UTC'
+USE_I18N = True
+USE_L10N = True
+USE_TZ = True
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
---
+++ django_saml2_auth/tests/test_utils.py
@@ -0,0 +1,87 @@
+import pytest
+from django_saml2_auth.utils import *
+
+
+def divide(a: int, b: int = 1) -> int:
+    return int(a/b)
+
+
+def hello(_: HttpRequest) -> HttpResponse:
+    return HttpResponse(content="Hello, world!")
+
+
+def goodbye(_: HttpRequest) -> HttpResponse:
+    raise SAMLAuthError("Goodbye, world!", extra={
+        "exc": RuntimeError("World not found!"),
+        "exc_type": RuntimeError,
+        "error_code": 0,
+        "reason": "Internal world error!",
+        "status_code": 500
+    })
+
+
+def test_run_hook_success():
+    result = run_hook("django_saml2_auth.tests.test_utils.divide", 2, b=2)
+    assert result == 1
+
+
+def test_run_hook_no_function_path():
+    with pytest.raises(SAMLAuthError) as exc_info:
+        run_hook("")
+        run_hook(None)
+
+    assert str(exc_info.value) == "function_path isn't specified"
+
+
+def test_run_hook_nothing_to_import():
+    with pytest.raises(SAMLAuthError) as exc_info:
+        run_hook("divide")
+
+    assert str(exc_info.value) == "There's nothing to import. Check your hook's import path!"
+
+
+def test_run_hook_import_error():
+    with pytest.raises(SAMLAuthError) as exc_info:
+        run_hook("django_saml2_auth.tests.test_utils.nonexistent_divide", 2, b=2)
+
+    assert str(exc_info.value) == (
+        "module 'django_saml2_auth.tests.test_utils' has no attribute 'nonexistent_divide'")
+    assert isinstance(exc_info.value.extra["exc"], AttributeError)
+    assert exc_info.value.extra["exc_type"] == AttributeError
+
+
+def test_run_hook_division_by_zero():
+    with pytest.raises(SAMLAuthError) as exc_info:
+        run_hook("django_saml2_auth.tests.test_utils.divide", 2, b=0)
+
+    assert str(exc_info.value) == "division by zero"
+    # Actually a ZeroDivisionError wrapped in SAMLAuthError
+    assert isinstance(exc_info.value.extra["exc"], ZeroDivisionError)
+    assert exc_info.value.extra["exc_type"] == ZeroDivisionError
+
+
+def test_get_reverse_success():
+    result = get_reverse("acs")
+    assert result == "/acs/"
+
+
+def test_get_reverse_no_reverse_match():
+    with pytest.raises(SAMLAuthError) as exc_info:
+        get_reverse("nonexistent_view")
+
+    assert str(exc_info.value) == "We got a URL reverse issue: ['nonexistent_view']"
+    assert issubclass(exc_info.value.extra["exc_type"], NoReverseMatch)
+
+
+def test_exception_handler_success():
+    decorated_hello = exception_handler(hello)
+    result = decorated_hello(HttpRequest())
+    assert result.content.decode("utf-8") == "Hello, world!"
+
+
+def test_exception_handler_handle_exception():
+    decorated_goodbye = exception_handler(goodbye)
+    result = decorated_goodbye(HttpRequest())
+    contents = result.content.decode("utf-8")
+    assert result.status_code == 500
+    assert "Reason: Internal world error!" in contents
